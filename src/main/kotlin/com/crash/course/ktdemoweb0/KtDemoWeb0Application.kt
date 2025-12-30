830038b137d898e86b5555a07d0c4900bb16f908
赵 鑫 <xinyi688life@gmail.com>
2025-12-30 10:37:39 +0800

    docker file

--- src/main/kotlin/com/crash/course/ktdemoweb0/KtDemoWeb0Application.kt
+++ src/main/kotlin/com/crash/course/ktdemoweb0/KtDemoWeb0Application.kt
@@ -33,4 +33,13 @@ class HomeController {
         Thread.startVirtualThread { log.info("i'm a virtual thread") }
         return "hi~"
     }
+
+    @GetMapping("/get")
+    suspend fun getPure(): String {
+        log.info("pure i'm a thread start ${Thread.currentThread().name}")
+        delay(1000)
+        log.info("pure i'm a thread end ${Thread.currentThread().name}")
+        Thread.startVirtualThread { log.info("pure i'm a virtual thread") }
+        return "pure hi~"
+    }
 }
